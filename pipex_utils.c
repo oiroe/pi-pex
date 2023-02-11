28456a7c944af7f6d4724c544d138278c0d2c833
Pathiti Boonprom <pboonpro@z2t13c3.42bangkok.com>
2023-02-12 01:26:06 +0700

    meaw

--- pipex_utils.c
+++ pipex_utils.c
@@ -6,13 +6,26 @@
 /*   By: pboonpro <pboonpro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 14:50:46 by pboonpro          #+#    #+#             */
-/*   Updated: 2023/02/04 22:44:16 by pboonpro         ###   ########.fr       */
+/*   Updated: 2023/02/12 00:56:52 by pboonpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	exe(char *cmd, char **env)
+void	my_free(char **str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		free(str[i]);
+		i++;
+	}
+	free(str);
+}
+
+char	*find_path(char *cmd, char **env)
 {
 	char	*path;
 	char	**my_path;
@@ -23,25 +36,61 @@ void	exe(char *cmd, char **env)
 	while (ft_strnstr(env[i], "PATH=", 5) == 0)
 		i++;
 	my_path = ft_split(env[i] + 5, ':');
-	/*i = 0;
-	while (my_path[i])
-		printf("%s\n", my_path[i++]);*/
+	i = 0;
 	while (my_path[i])
 	{
 		j_path = ft_strjoin(my_path[i], "/");
 		path = ft_strjoin(j_path, cmd);
 		free(j_path);
-if (access)
+		if (access(path, F_OK) == 0)
+		{
+			my_free(my_path);
+			return (path);
+		}
+		free(path);
 		i++;
 	}
+	my_free(my_path);
+	return (0);
 }
 
-int	main (int argc, char **argv, char **env)
+void	exe(char *av, char **env)
 {
-	exe(argv, env);
-	int i = 0;
-	/*while(env[i])
-		printf("%s\n", env[i++]);*/
+	char	**cmd;
+	char	*path;
+
+	cmd = ft_split(av, ' ');
+	path = find_path(cmd[0], env);
+	if (!path)
+	{
+		my_free(cmd);
+		error();
+	}
+	if (execve(path, cmd, env) != 0)
+		error();
+}
+
+/*int	test(void)
+{
+	return (1);
+	int	i;
+	i = 0;
+	while (i < 10)
+	{
+		printf("%d ", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}*/
+
+int	main (int ac, char **av, char **env)
+{
+	//exe("which ls", env);
+	exe(av[1], env);
+	//int i = 0;
+	//while(env[i])
+		//printf("%s\n", env[i++]);
 
 	return (0);
 }
